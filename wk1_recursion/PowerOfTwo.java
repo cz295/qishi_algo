cea57daaa00a8504f3ba8efd74f37d9c754d054a
LiangchenLiu <80559576+LiangchenLiu@users.noreply.github.com>
2023-06-26 23:06:35 -0500

    Delete PowerOfTwo.java

--- wk1_recursion/PowerOfTwo.java
+++
@@ -1,9 +0,0 @@
-package wk1_recursion;
-
-public class PowerOfTwo {
-    public boolean isPowerOfTwo(int n) {
-        if (n <= 0) return false;
-        if (n < 2) return true;
-        return n % 2 == 0 && isPowerOfTwo(n / 2);
-    }
-}
